1c144efe4d6cc6a066eefba8f74d98a72fd3c5e7
Mark Music <markmusic999@gmail.com>
2020-08-21 18:34:45 -0600

    Added Notes

--- typescript/learning_projects/fetch_json/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var axios_1 = require('axios');
-var url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios_1['default'].get(url).then(function (response) {
-  console.log(response.data);
-});
