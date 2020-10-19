9775d2d576837676eb0f3855048da2fac07b5b9f
Mark Music <markmusic999@gmailcom>
2020-10-19 09:40:44 -0600

    feat(magic_8_ball): 🐝 Finished Magic 8 Ball

--- flutter-dart/projects/magic_8_ball/lib/main.dart
+++ flutter-dart/projects/magic_8_ball/lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MainPage(),
       ),
     );
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Color(0xff0A2463),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          print("I got clicked");
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Center(
+          child: Image.asset("images/ball$ballNumber.png"),
+        ),
+      ),
+    );
+  }
+}
